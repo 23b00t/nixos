b1e4180bf8d993deef0ede41e4983aa25e08ebee
Daniel Kipp <daniel.kipp@gmail.com>
2025-06-21 19:44:50 +0200

    merging grafics example

--- vms/net/flake.nix
+++ vms/net/flake.nix
@@ -25,13 +25,67 @@
             microvm.nixosModules.microvm
             {
               networking.hostName = "net-vm";
-              users.users.root.password = "";
+              # users.users.root.password = "";
 
-              microvm.interfaces = [ {
-                id = "vm${toString index}";
-                type = "tap";
-                inherit mac;
-              } ];
+              microvm = {
+                hypervisor = "cloud-hypervisor";
+                graphics.enable = true;
+                interfaces = lib.optional (tapInterface != null) {
+                  id = "vm${toString index}";
+                  type = "tap";
+                  inherit mac;
+                };
+              };
+
+              system.stateVersion = lib.trivial.release;
+              nixpkgs.overlays = [ self.overlay ];
+
+              services.getty.autologinUser = "user";
+              users.users.user = {
+                password = "";
+                group = "user";
+                isNormalUser = true;
+                extraGroups = [ "wheel" "video" ];
+              };
+              users.groups.user = {};
+              security.sudo = {
+                enable = true;
+                wheelNeedsPassword = false;
+              };
+
+              environment.sessionVariables = {
+                WAYLAND_DISPLAY = "wayland-1";
+                DISPLAY = ":0";
+                QT_QPA_PLATFORM = "wayland"; # Qt Applications
+                GDK_BACKEND = "wayland"; # GTK Applications
+                XDG_SESSION_TYPE = "wayland"; # Electron Applications
+                SDL_VIDEODRIVER = "wayland";
+                CLUTTER_BACKEND = "wayland";
+              };
+
+              systemd.user.services.wayland-proxy = {
+                enable = true;
+                description = "Wayland Proxy";
+                serviceConfig = with pkgs; {
+                  # Environment = "WAYLAND_DISPLAY=wayland-1";
+                  ExecStart = "${wayland-proxy-virtwl}/bin/wayland-proxy-virtwl --virtio-gpu --x-display=0 --xwayland-binary=${xwayland}/bin/Xwayland";
+                  Restart = "on-failure";
+                  RestartSec = 5;
+                };
+                wantedBy = [ "default.target" ];
+              };
+
+              environment.systemPackages = with pkgs; [
+                xdg-utils # Required
+                firefox
+              ] ++ map (package:
+                lib.attrByPath (lib.splitString "." package) (throw "Package ${package} not found in nixpkgs") pkgs
+              ) (
+                builtins.filter (package:
+                  package != ""
+                ) (lib.splitString " " packages));
+
+              hardware.graphics.enable = true;
 
               networking.useNetworkd = true;
 
