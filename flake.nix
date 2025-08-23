c9def095bf94786b24766d580450427be465bf76
Daniel Kipp <daniel.kipp@gmail.com>
2025-08-23 15:29:37 +0200

    fixed flake.nix

--- flake.nix
+++ flake.nix
@@ -1,5 +1,5 @@
 {
-  description = "Nixos config";
+  description = "Nixos config by 23b00t";
 
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
@@ -7,31 +7,33 @@
       url = "github:nix-community/home-manager";
       inputs.nixpkgs.follows = "nixpkgs";
     };
-    # --- NEUER BLOCK ---
-    # Füge dein lazyvim-Repo als Input hinzu.
-    # Nix wird diesen Ordner jetzt garantiert kennen.
+    # Dein lazyvim-Repo als garantierte, externe Quelle
     lazyvim-config = {
       url = "github:23b00t/lazyvim";
-      flake = false; # Wichtig, da dein lazyvim-Repo kein Flake ist
+      flake = false;
     };
-    # --- ENDE NEUER BLOCK ---
   };
 
   outputs = { self, nixpkgs, home-manager, lazyvim-config, ... }@inputs: {
-    nixosConfigurations.d = nixpkgs.lib.nixosSystem {
+    # KORREKTUR: Der Name hier MUSS mit deinem Hostnamen übereinstimmen.
+    nixosConfigurations.machine = nixpkgs.lib.nixosSystem {
       system = "x86_64-linux";
-      specialArgs = {
-        # Gib den neuen Input an deine Konfiguration weiter
-        inherit lazyvim-config;
-      };
+      
       modules = [
+        # Der Pfad zu deiner Systemkonfiguration
         ./machines/d/configuration.nix
-        home-manager.nixosModules.home-manager {
+
+        # KORREKTUR: Home-Manager wird als Modul mit den richtigen Argumenten eingebunden
+        home-manager.nixosModules.home-manager
+        {
           home-manager.useGlobalPkgs = true;
           home-manager.useUserPackages = true;
+          
+          # Hier wird deine home.nix importiert
           home-manager.users.nx = import ./home/home.nix;
+
+          # KORREKTUR: So wird `lazyvim-config` korrekt an `home.nix` übergeben
           home-manager.extraSpecialArgs = {
-            # Gib den Input auch an Home-Manager weiter
             inherit lazyvim-config;
           };
         }
