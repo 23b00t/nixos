778d59469956ab0449c9ef9245af9a562b455374
Daniel Kipp <daniel.kipp@gmail.com>
2025-10-08 19:12:03 +0200

    fix channel

--- flake.nix
+++ flake.nix
@@ -2,7 +2,7 @@
   description = "Nixos config by 23b00t";
 
   inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs/nixos";
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-24.05";
     home-manager = {
       url = "github:nix-community/home-manager";
       inputs.nixpkgs.follows = "nixpkgs";
