51f322ee9ce0f8964e56228133bb125cdcf1963b
Daniel Kipp <daniel.kipp@gmail.com>
2025-10-09 19:23:20 +0200

    remove not needed tools

--- home/home.nix
+++ home/home.nix
@@ -51,18 +51,9 @@
     nix-output-monitor
 
     btop # replacement of htop/nmon
-    iotop # io monitoring
-    iftop # network monitoring
-
-    # system call monitoring
-    strace # system call monitoring
-    ltrace # library call monitoring
-    lsof # list open files
 
     # system tools
-    sysstat
     lm_sensors # for `sensors` command
-    ethtool
     pciutils # lspci
     usbutils # lsusb
 
